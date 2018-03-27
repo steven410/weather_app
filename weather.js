cb5446725dfa0c6318b5842a86e8a769997886b3
steven410 <stevenwh2012@gmail.com>
2018-03-26 20:29:22 -0500

    added temp unit switching

--- weather.js
+++ weather.js
@@ -1,8 +1,8 @@
-var api = "https://fcc-weather-api.glitch.me/api/current?";
-var lat, lon;
-
 $( document ).ready(function() {
- 
+$(".optional-temp").click(function(){
+        $(".temp").toggleClass("hidden");
+        $(".optional-temp").toggleClass("alt");
+    });
 if (navigator.geolocation) {
 		navigator.geolocation.getCurrentPosition(function(position) {
 			var lat = "lat=" + position.coords.latitude;
@@ -10,27 +10,23 @@ if (navigator.geolocation) {
 			getWeather(lat, lon);
 		});
 	} else alert("Geolocation is not supported by this browser.");
-	
 });
 
 function getWeather(lat, lon) {
-	//var urlString = api + lat + "&" + lon;
 	$.ajax({
-		url: api + lat + "&" + lon,
+		url: "https://fcc-weather-api.glitch.me/api/current?" + lat + "&" + lon,
 		success: function(result) {
-    
-      var desc = result.weather[0].main;
-			$("#temp").text(Math.round(result.main.temp));
+      		var desc = result.weather[0].main;
+	  		$("#temp").text(Math.round(result.main.temp));
 			$("#city").text(result.name);
 			$("#country").text(result.sys.country);
 			$("#desc").text(desc);
       		$('.wi-day-' + desc.toLowerCase()).removeClass('hidden');
+      		$("#temp-converted").text(Math.round(result.main.temp * 9 / 5 + 32));
 			}
 		});
 }
 
-// function convert(){
-// 	result.main.temp * 9 / 5 + 32
-// }
+
 
 
