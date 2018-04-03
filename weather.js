94a1319b8d93739d4a2abcf8b76947195bf15ab6
steven410 <stevenwh2012@gmail.com>
2018-04-02 21:00:41 -0500

    first pass at replacing jQuery with Fetch API

--- weather.js
+++ weather.js
@@ -5,28 +5,50 @@ $(".optional-temp").click(function(){
     });
 if (navigator.geolocation) {
 		navigator.geolocation.getCurrentPosition(function(position) {
-			var lat = "lat=" + position.coords.latitude;
-			var lon = "lon=" + position.coords.longitude;
+			const lat = "lat=" + position.coords.latitude;
+			const lon = "lon=" + position.coords.longitude;
 			getWeather(lat, lon);
 		});
 	} else alert("Geolocation is not supported by this browser.");
 });
 
+// function getWeather(lat, lon) {
+// 	$.ajax({
+// 		url: "https://fcc-weather-api.glitch.me/api/current?" + lat + "&" + lon,
+// 		success: function(result) {
+//       		const desc = result.weather[0].main;
+// 	  		$("#c").text(Math.round(result.main.temp));
+// 	  		$("#f").text(Math.round(result.main.temp * 9 / 5 + 32));
+// 			$("#city").text(result.name);
+// 			$("#country").text(result.sys.country);
+// 			$("#desc").text(desc);
+//       		$('.wi-day-' + desc.toLowerCase()).removeClass('hidden');
+// 			}
+// 		});
+// }
+
+//FETCH API
 function getWeather(lat, lon) {
-	$.ajax({
-		url: "https://fcc-weather-api.glitch.me/api/current?" + lat + "&" + lon,
-		success: function(result) {
-      		var desc = result.weather[0].main;
-	  		$("#temp").text(Math.round(result.main.temp));
-			$("#city").text(result.name);
-			$("#country").text(result.sys.country);
-			$("#desc").text(desc);
-      		$('.wi-day-' + desc.toLowerCase()).removeClass('hidden');
-      		$("#temp-converted").text(Math.round(result.main.temp * 9 / 5 + 32));
-			}
-		});
+let url = "https://fcc-weather-api.glitch.me/api/current?" + lat + "&" + lon;
+fetch(url)
+  .then(response => response.json())
+  .then(data => {
+    // Here's a list of repos!
+    console.log(data)
+  });
 }
 
-
-
-
+// function getWeather(lat, lon) {
+// fetch("https://fcc-weather-api.glitch.me/api/current?" + lat + "&" + lon)
+//   .then(resp => resp.json());
+//   .then(data => {
+//   	console.log(data)
+//       		const desc = data.weather[0].main;
+// 	  		document.querySelector("#c").innerHTML(Math.round(data.main.temp));
+// 	  		document.querySelector("#f").innerHTML(Math.round(data.main.temp * 9 / 5 + 32));
+// 			document.querySelector("#city").innerHTML(data.name);
+// 			document.querySelector("#country").innerHTML(data.sys.country);
+// 			document.querySelector("#desc").innerHTML(desc);
+//       		document.querySelector('.wi-day-' + desc.toLowerCase()).classList.remove('hidden');
+// 			});
+// }
